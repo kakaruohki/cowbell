9ce91213c24fbb8e6392a8e2a5d030e4967f81b3
kakeruohki <kakeru.ohki@gmail.com>
2020-01-23 14:42:26 +0900

    標準出力削除

--- check_price.rb
+++ check_price.rb
@@ -3,7 +3,5 @@ require_relative "selenium_helper"
 require_relative "rakuten/rakuten_parser"
 require_relative "qoo10/qoo10_parser"
 
-puts "## start ,#{Time.now}"
 Qoo10.new.check_price
 Rakuten.new.check_price
-puts "## end ,#{Time.now}"
